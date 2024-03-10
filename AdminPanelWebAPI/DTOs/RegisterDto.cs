a288e454f0f7a09674d1ec1a3b2d08f44edd02f1
codingismyjourney <79038618+codingismyjourney@users.noreply.github.com>
2024-03-10 23:02:49 +0530

    - password data anotation added

--- AdminPanelWebAPI/DTOs/RegisterDto.cs
+++ AdminPanelWebAPI/DTOs/RegisterDto.cs
@@ -18,7 +18,11 @@ public class RegisterDto
     [Phone]
     public string PhoneNumber { get; set; }
 
-    [Required]
+    [Required(ErrorMessage = "Password is required")]
+    [DataType(DataType.Password)]
+    [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+    [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,}$", ErrorMessage = "Password must contain at least one lowercase letter, one uppercase letter, one digit, and one special character.")]
+    public string Password { get; set; }
     public string Password { get; set; }
 
     [Required]
